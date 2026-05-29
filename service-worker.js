8b7051baef2e265c8a76930e9e2553906efccf9f
henrykamsi <kamsih924@gmail.com>
2026-05-29 11:11:29 +0100

    Refactor service worker caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,7 @@
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
-
 self.addEventListener('install', (e) => {
-    self.skipWaiting();
+    e.waitUntil(caches.open('hgt-store').then((cache) => cache.addAll(['./'])));
 });
 
 self.addEventListener('fetch', (e) => {
-    if (e.request.url.includes('api.openweathermap.org') || e.request.url.includes('onesignal')) return;
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
 });
-
